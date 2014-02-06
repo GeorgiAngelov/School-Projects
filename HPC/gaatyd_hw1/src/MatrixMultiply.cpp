116f1a8be7c121b299673ab70edcfef026078db5
gaatyd_112113 <gaatyd@babbage-lb2.cs.missouri.edu>
2014-02-06 15:45:09 -0600

    HW1 - increased performance by accessing the matrices using pointers

--- HPC/gaatyd_hw1/src/MatrixMultiply.cpp
+++ HPC/gaatyd_hw1/src/MatrixMultiply.cpp
@@ -22,17 +22,34 @@ scottgs::FloatMatrix scottgs::MatrixMultiply::operator()(const scottgs::FloatMat
 	if (lhs.size2() != rhs.size1())
 		throw std::logic_error("matrix incompatible lhs.size2() != rhs.size1()");
 
+	//create the matrix
 	scottgs::FloatMatrix result(lhs.size1(),rhs.size2());
-	unsigned int i;
-	unsigned int j;
-	unsigned int k;
-	// YOUR ALGORIHM WITH COMMENTS GOES HERE:
-	for (i = 0; i < lhs.size1(); ++i)
-        for (j = 0; j < rhs.size2(); ++j)
-			for (k = 0; k < lhs.size2(); ++k) 		
-				result(i,j) += lhs(i,k) * rhs(k,j);
 	
+	//create the unsigned ints used for the three for loops
+	unsigned int m1_c_i; //matrix 1 column index
+	unsigned int m1_r_i; //matrix 1 row index
+	unsigned int m2_c_i; //matrix 2 column index
 	
+	//get the sizes I need and put them into a constant
+	const unsigned int m1_num_row = lhs.size1(); //# of row of result matrix
+	const unsigned int m1_num_col = lhs.size2();
+	const unsigned int m2_num_col = rhs.size2(); //# of column of result matrix
+	
+	//get a reference of the matrix's first element ( this will be a pointer to the first element )
+	const float *m1 = &lhs(0,0);
+	const float *m2 = &rhs(0,0);
+	
+	//get a copy of the first element.
+	float *r = &result(0,0);
+	
+	//loop through each row of matrix 1
+	for (m1_c_i = 0; m1_c_i < m1_num_row; ++m1_c_i)
+		//loop through each column of matrix 2
+        for (m1_r_i = 0; m1_r_i < m2_num_col; ++m1_r_i)
+			//loop through each column of matrix 1
+			for (m2_c_i = 0; m2_c_i < m1_num_col; ++m2_c_i)
+				*(r + m1_c_i*m2_num_col + m1_r_i) += *(m1 + m1_c_i*m1_num_col + m2_c_i) * *(m2 + m2_c_i*m2_num_col + m1_r_i);
+				
 	return result;
 }
 
