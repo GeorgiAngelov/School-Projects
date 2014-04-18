b7204d5665401c659ab89bfedec3d457e7318662
gaatyd_112113 <gaatyd@babbage2.cs.missouri.edu>
2014-04-17 22:39:02 -0500

    sending two elements in a struct

--- HPC/gaatyd_hw5/main.cpp
+++ HPC/gaatyd_hw5/main.cpp
@@ -6,27 +6,56 @@ int main(int argc, char *argv[]){
 	//initialize MPI
 	MPI_Init(&argc, &argv);
 	
+	message_to_worker message;
+	//declare my new type
+	MPI_Datatype MessageType;
+	
+	//declare the types the the structure will have
+	//MPI_Datatype type[3] = { MPI_CHAR, MPI_DOUBLE, MPI_CHAR };
+	MPI_Datatype type[2] = { MPI_INT, MPI_INT };
+	//the number of results for each type(int[50] will be 50 etc..)
+	//int blocklen[3] = { 1, 6, 7 };
+	int blocklen[2] = {1,1};
+	
+	//this will store the displacement for each var in the structure
+	//MPI_Aint disp[3];
+	MPI_Aint disp[2] = {0,0};
+	MPI_Aint var1, var2;
+	
+	//get the address of the message and put in var1(to used for calculating displacement)
+	/*MPI_Get_address(&(message), &var1);
+	MPI_Get_address(&(message.N), &var2);*/
+	
+	//number of vars
+	int count = 2;
+	
+	//define the type
+	//MPI_Type_create_struct(3, blocklen, disp, type, &Particletype);
+    //MPI_Type_commit(&MessageType);
+	MPI_Type_struct(count, blocklen, disp, type, &MessageType);
+	MPI_Type_commit(&MessageType); 
+
+	std::cout << "The displacement is " << disp[0] << std::endl;
+	
 	int rank;
 	int world_size;
-	
-	message_to_worker message;
 	
 	//give me my current rank(node 1 reiceves 1 , node 2 etc... )
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	
 	int counter = 1;
+	message.N = 5;
+	message.seed = 12312;
 	
 	//IF MASTER
 	if (rank == 0) {
-		int value = 5;
 		int result;
 		
 		//send messages to the workers
-		while(counter<world_size){
-			value = counter*value;
-			//send the message of type int to 
-			result = MPI_Ssend(&value, 1, MPI_INT, counter, 0, MPI_COMM_WORLD);
+		while(counter<2){
+			//send 1 message of type MessageType using my own structure(first parameter)
+			result = MPI_Ssend(&message, 1, MessageType, counter, tag, MPI_COMM_WORLD);
 			
 			//make sure the mssage was sent correctly
 			if (result == MPI_SUCCESS){
@@ -40,19 +69,17 @@ int main(int argc, char *argv[]){
 	}
 	//IF WORKER
 	else if (rank > 0) {
-		int value;
 		//the worker will wait until it receives a message
-		int result = MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+		int result = MPI_Recv(&message, 1, MessageType, 0, tag, MPI_COMM_WORLD,
 				  MPI_STATUS_IGNORE);
 				  
 		//make sure the message was RECEIVED correctly
 		if (result == MPI_SUCCESS){
-			std::cout << "Worker - Rank: " << rank <<" OK! and received value " << value << std::endl;
+			std::cout << "Worker - Rank: " << rank <<" OK! and the value received through the message is " << message.N << " and seed is " << message.seed << std::endl;
 		}
 	}
 	
 	//shutdown MPI
 	MPI_Finalize();
 	return 0;
-	
 }
--- HPC/gaatyd_hw5/main.hpp
+++ HPC/gaatyd_hw5/main.hpp
@@ -1,3 +1,6 @@
 typedef struct _message_to_worker{
 	int N;
+	int seed;
 }message_to_worker;
+
+int tag = 123;
